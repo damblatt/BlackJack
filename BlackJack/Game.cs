99b07f6985cfb8ebf17a48fb1998873dc5b01199
damblatt <dam.blatt@gmail.com>
2023-09-29 17:32:14 +0200

    I created the file structure and added a table that contains the current rounds player and the bet they made.

---
+++ BlackJack/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BlackJack
+{
+    class Game
+    {
+        public List<Player> PlayerList { get; set; } = new List<Player>();
+        public Croupier Croupier { get; set; } = new Croupier();
+        private DataTable bets = new DataTable();
+
+        public Game(int playerAmount) {
+            for (int i = 0; i < playerAmount; i++)
+            {
+                AddPlayer(i);
+            }
+        }
+
+        public void Start()
+        {
+            PrintTitle();
+
+            GameLoop();
+        }
+
+        private void GameLoop()
+        {
+            bets.Clear();
+            bets.Columns.Add("Player", typeof(Player));
+            bets.Columns.Add("Bet", typeof(int));
+
+            foreach (Player player in PlayerList)
+            {
+                int bet = player.GetBet();
+                bets.Rows.Add(player, bet);
+            }
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        private void PrintTitle()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------------\n|      Black Jack      |\n------------------------");
+        }
+
+        private void AddPlayer(int id)
+        {
+            Player player = new Player(id);
+            PlayerList.Add(player);
+        }
+
+        private void DistributeCards()
+        {
+
+        }
+    }
+}
---
+++ BlackJack/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Player
+    {
+        public int Id;
+        public int Bankroll;
+        public List<Card> Cards;
+        
+        public Player(int id)
+        {
+            Id = id;
+        }
+
+        public int GetBet()
+        {
+            Console.WriteLine($"You have {Bankroll} Credits in your bankroll. How much would you like to bet?");
+            return Int32.Parse(Console.ReadLine());
+            //TODO: Check if the player has enough credits in his bankroll. If not, ask him for a new bet.
+        }
+    }
+}
